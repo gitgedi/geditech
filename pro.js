9af333ac2b3dc2e94b8698c0417b0b965ea1950b
gitgedi <165547283+gitgedi@users.noreply.github.com>
2024-03-31 13:22:59 +0300

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.b', { viewFactor: 0 });
+var slideUp = {
+    distance: '450%',
+    origin: 'bottom',
+    opacity: 1
+  
+};
+
+
+ScrollReveal().reveal('.home', { scale: 0.9 });
+ScrollReveal().reveal('.c', slideUp);
+ScrollReveal().reveal('.d', slideUp);
